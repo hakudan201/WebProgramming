f83635a707c6898cb7640b041f1f6fb5e65890db
Dang Huy Hoang <doiquanhieumk@gmail.com>
2022-11-01 20:26:13 +0700

    hotfix

--- Lab3/DateTimeFunction.php
+++ Lab3/DateTimeFunction.php
@@ -6,23 +6,6 @@
         <font size="5" color="blue">DATE TIME FUNCTION</font>
         <br>
         <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="GET">
-        <?php
-            if (array_key_exists("date1", $_GET)) {
-                $date1 = $_GET["date1"];
-                $month1 = $_GET["month1"];
-                $year1 = $_GET["year1"];
-                $date2 = $_GET["date2"];
-                $month2 = $_GET["month2"];
-                $year2 = $_GET["year2"];
-            } else {
-                $date1 = 1;
-                $month1 = 1;
-                $year1 = 2000;
-                $date2 = 1;
-                $month2 = 1;
-                $year2 = 2000;
-            }
-        ?>
         <table>
             <tr>
                 <td>Name 1: </td>
@@ -33,41 +16,8 @@
         </table>
         <table>
             <tr>
-                <td>Birthday: </td>
                 <td>
-                    <select name="date1">
-                        <?php
-                            for ($i=1; $i<=31; $i++) {
-                                if ($date1==$i) {
-                                    print ("<option selected>$i</option>");
-                                } else {
-                                    print ("<option>$i</option>");
-                                }
-                            }
-                        ?>
-                    </select>
-                    <select name="month1">
-                        <?php
-                            for ($i=1; $i <=12; $i++) {
-                                if ($month1==$i) {
-                                    printf("<option selected>$i</option>");
-                                } else {
-                                    printf("<option>$i</option>");
-                                }
-                            }
-                        ?>
-                    </select>
-                    <select name="year1">
-                        <?php
-                            for ($i=1; $i <=3000; $i++) {
-                                if ($year1==$i) {
-                                    printf("<option selected>$i</option>");
-                                } else {
-                                    printf("<option>$i</option>");
-                                }
-                            }
-                        ?>
-                    </select>
+                    Birthday: <input type="date" placeholder="MM/DD/YYYY" name="birth1" required>
                 </td>
             </tr>
             <table>
@@ -80,41 +30,8 @@
         </table>
         <table>
             <tr>
-                <td>Birthday: </td>
                 <td>
-                    <select name="date2">
-                        <?php
-                            for ($i=1; $i<=31; $i++) {
-                                if ($date2==$i) {
-                                    print ("<option selected>$i</option>");
-                                } else {
-                                    print ("<option>$i</option>");
-                                }
-                            }
-                        ?>
-                    </select>
-                    <select name="month2">
-                        <?php
-                            for ($i=1; $i <=12; $i++) {
-                                if ($month2==$i) {
-                                    printf("<option selected>$i</option>");
-                                } else {
-                                    printf("<option>$i</option>");
-                                }
-                            }
-                        ?>
-                    </select>
-                    <select name="year2">
-                        <?php
-                            for ($i=1; $i <=3000; $i++) {
-                                if ($year2==$i) {
-                                    printf("<option selected>$i</option>");
-                                } else {
-                                    printf("<option>$i</option>");
-                                }
-                            }
-                        ?>
-                    </select>
+                    Birthday: <input type="date" placeholder="MM/DD/YYYY" name="birth2" required>
                 </td>
             </tr>
         </table>
@@ -127,10 +44,9 @@
         <table>
             <?php
                 if ($_GET["name1"] != "" && $_GET["name2"] != ""){
-                    $timestamp1 = date_create("$year1-$month1-$date1");
-                    $timestamp2 = date_create("$year2-$month2-$date2");
+                    $timestamp1 = date_create($_GET["birth1"]);
+                    $timestamp2 = date_create($_GET["birth2"]);
                     echo "Hello " . $_GET["name1"] . " and " . $_GET["name2"] . "<br><br>";
-                    if (checkdate($_GET["month1"], $_GET["date1"], $_GET["year1"]) == TRUE && checkdate($_GET["month2"], $_GET["date2"], $_GET["year2"]) == TRUE){
                         echo $_GET["name1"] . "'s birthday is " . date_format($timestamp1,"l, F d, Y") . "<br>";
                         echo $_GET["name2"] . "'s birthday is " . date_format($timestamp2,"l, F d, Y") . "<br>";
                         $interval1 = date_diff($timestamp1, $timestamp2);
@@ -148,9 +64,6 @@
                             $interval2 *= -1;
                                 echo "Difference between two persons is " . $interval2 . " years";
                             }
-                    } else {
-                        echo "You have entered invalid date";
-                    }
                 }
             ?>
         </table>
